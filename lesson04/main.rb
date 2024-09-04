d8834e556c44b1985bfcf76bad5d8f466e11b51b
Asya Nickolskaya <nickolskaya@gmail.com>
2024-09-04 09:48:24 +0300

    feat: add main

---
+++ lesson04/main.rb
@@ -0,0 +1,9 @@
+require_relative "wagon"
+require_relative 'wagon_types'
+require_relative "station"
+require_relative "train"
+require_relative "route"
+require_relative "interface"
+
+interface = Interface.new
+interface.menu
