b960a8b9bcdc92fea7c8b60344d5ff47f1ea1eca
Asya Nickolskaya <nickolskaya@gmail.com>
2024-09-07 10:02:33 +0300

    refactor

--- lesson04/interface.rb
+++ lesson04/interface.rb
@@ -82,7 +82,7 @@ class Interface
     from = gets.chomp
     puts "Выберите конечную станцию:"
     to = gets.chomp
-    routes << Route.new(@stations[from], @stations[to])
+    routes << Route.new(stations[from], stations[to])
     puts "Маршрут #{from} - #{to} создан"
   end
 
@@ -91,40 +91,98 @@ class Interface
     puts "1. Добавить станцию"
     puts "2. Удалить станцию"
     action = gets.chomp.to_i
-    if action == 1
-      add_station
-    else
-      remove_station
+  
+    case action
+      when 1
+        add_station_to_route
+      when 2
+        remove_station_from_route
+      else
+        puts "Некорректный ввод"
     end
+  end
 
-    def print_route
-      route.stations.each { |station, i| puts "#{i + 1}: #{station.name}" }
+  def print_routes
+    routes.each_with_index do |route, i| 
+      puts "#{i}: #{route.stations.first.name} - #{route.stations.last.name}"
     end
+  end
 
-    def add_station
-      puts "Выберите станцию:"
-      print_route
-      index = gets.chomp.to_i - 1
-      route.add_station(@stations[index])
-      puts "Станция #{station} добавлена в маршрут"
+    
+  def print_route_stations(route)
+    route.stations.each_with_index do |station, i|
+      puts "#{i}: #{station.name}"
     end
+  end
 
-    def remove_station
-      puts "Выберите станцию:"
-      print_route
-      index = gets.chomp.to_i - 1
-      route.remove_station(@stations[index])
-      puts "Станция #{station} удалена из маршрута"
-    end
+  def add_station_to_route
+    puts "Выберите маршрут:"
+    print_routes
+    choice = gets.chomp.to_i
+    route = routes[choice]
+    return if choise.negative? || choise > routes.size
+  
+    puts "Выберите станцию:"
+    print_route_stations(route)
+    station_index = gets.chomp.to_i
+    station = @stations[station_index]
+  
+    route.add_station(station)
+    puts "Станция #{station.name} добавлена в маршрут"
+  end
+
+  def remove_station_from_route
+    puts "Выберите маршрут:"
+    print_routes
+    choice = gets.chomp.to_i
+    route = routes[choice]
+    
+    puts "Выберите станцию:"
+    print_route_stations(route)
+    station_index = gets.chomp.to_i
+    station = route.stations[station_index]
+  
+    route.remove_station(station)
+    puts "Станция #{station.name} удалена из маршрута"
   end
 
   def assign_route
+    puts "Выберите маршрут:"
+    print_routes
+    route = gets.chomp.to_i
+
+    train = choose_train
+    train.assign_route(routes[route])
   end
 
-  def add_wagon
+  def choose_train
+    puts 'Выберите поезд:'
+    trains.each_with_index { |train, index| puts "#{index}: #{train.number}" }
+    input = gets.to_i
+    return if input < 0 || input >= trains.size
+    trains[input]
+  end
+
+  def add_wagon(wagon = nil)
+    choose_train
+    puts "Выберите вагон:"
+    puts "1. Пассажирский"
+    puts "2. Грузовой"
+    type = gets.chomp.to_i
+
+    case type
+      when 1
+        wagon = PassengerWagon.new
+      when 2
+        wagon = CargoWagon.new
+    end
+
+    trains[train].add_wagon(wagon)
   end
 
   def remove_wagon
+    train =choose_train
+    trains[train].remove_wagon
   end
 
   def move_train
