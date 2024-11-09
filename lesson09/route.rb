4bd93cd345e60e52922354462f748ecbe91b7966
Asya Nickolskaya <nickolskaya@gmail.com>
2024-11-09 11:58:31 +0200

    fix: route

--- lesson09/route.rb
+++ lesson09/route.rb
@@ -1,10 +1,13 @@
 require './modules/instance_counter'
-require './modules/validate'
+require './modules/validation'
 
 class Route
   include InstanceCounter
-  include Validate
+  include Validation
   attr_reader :stations
+
+  validate :start, :type, Station
+  validate :finish, :type, Station
 
   def initialize(start, finish)
     @start = start
@@ -20,10 +23,12 @@ class Route
     return if @points.detect { |station| station.name == point.name }
 
     @points << point
+    @stations = [@start, @points, @finish].flatten
   end
 
   def remove_station(point)
     @points.delete(point)
+    @stations = [@start, @points, @finish].flatten
   end
 
   def print
@@ -32,11 +37,5 @@ class Route
     else
       puts "#{@start.name} - #{@points.map(&:name).join(' - ')} - #{@finish.name}"
     end
-  end
-
-  private
-
-  def validate!
-    raise 'Начальная станция не может быть конечной' if @start == @finish
   end
 end
