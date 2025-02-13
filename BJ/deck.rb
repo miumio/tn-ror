55feac4a56affd299b8cc21c175ac547ee1e03d8
Asya Nickolskaya <nickolskaya@gmail.com>
2025-02-13 22:14:10 +0200

    add deck

---
+++ BJ/deck.rb
@@ -0,0 +1,27 @@
+require "./card"
+
+class Deck
+  RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K, :A]
+  SUITS = ['❤︎', '♠', '♦', '♣']
+
+  def initialize
+    @cards = []
+    generate_deck
+    shuffle
+  end
+
+  def take_card
+    generate_deck if @cards.empty?
+    @cards.pop
+  end
+
+  private
+
+  def generate_deck
+    @cards = SUITS.product(RANKS).map { |suit, value| Card.new(suit, value) }
+  end
+
+  def shuffle
+    @cards.shuffle!
+  end
+end
